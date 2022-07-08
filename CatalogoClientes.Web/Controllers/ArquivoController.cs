f0c516aacb378ff4812fdb44f388c6cd0a4e125a
rjsilvaoficial <rjsilvaoficial@gmail.com>
2022-07-08 18:35:40 -0300

    Adicionado Crud via scaffold e configurada view e controller para receber também o arquivo de foto na criação de entidade Cliente

---
+++ CatalogoClientes.Web/Controllers/ArquivoController.cs
@@ -0,0 +1,23 @@
+﻿using CatalogoClientes.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatalogoClientes.Web.Controllers
+{
+    public class ArquivoController : Controller
+    {
+        // GET: Arquivo
+        public ActionResult ExibirImagem(int id)
+        {
+            using(var db = new ClienteContexto())
+            {
+                var arquivoRetorno = db.Clientes.FirstOrDefault(c => c.ClienteId == id);
+                return File(arquivoRetorno.Imagem, arquivoRetorno.ImagemTipo);
+            }
+
+        }
+    }
+}
--- CatalogoClientes.Web/Controllers/ClienteController.cs
+++ CatalogoClientes.Web/Controllers/ClienteController.cs
@@ -1,33 +1,157 @@
-﻿using CatalogoClientes.Dominio.Entidades;
-using CatalogoClientes.Dominio.Repositorio;
-using PagedList;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using CatalogoClientes.Dominio.Entidades;
+using CatalogoClientes.Dominio.Repositorio;
+using PagedList;
 
 namespace CatalogoClientes.Web.Controllers
 {
     public class ClienteController : Controller
     {
-        private IRepositorio<Cliente> _repositorioCliente;
+        private readonly IRepositorio<Cliente> _repositorio;
+        private ClienteContexto db = new ClienteContexto();
 
         public ClienteController()
         {
-            _repositorioCliente = new ClienteRepositorio(new ClienteContexto());
+            _repositorio = new ClienteRepositorio(new ClienteContexto());
         }
-        // GET: Cliente
-        public ActionResult Index()
+
+        //GET: Cliente
+        public ActionResult Catalogo(int? pagina)
+        {
+            int numPagina = pagina ?? 1;
+            int tamPagina = 1;
+            return View(_repositorio.GetTodos().ToPagedList(numPagina, tamPagina));
+        }
+
+        //// GET: Cliente
+        //public ActionResult Catalogo()
+        //{
+        //    return View(db.Clientes.ToList());
+        //}
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
         {
             return View();
         }
 
-        public ActionResult Catalogo(int? pagina)
+        // POST: Cliente/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(
+            [Bind(Include = "ClienteId,Nome,Email,Endereco,Imagem,ImagemTipo")] 
+                Cliente cliente,HttpPostedFileBase upload )
         {
-            int tamPagina = 1;
-            int numPagina = pagina ?? 1;
-            return View(_repositorioCliente.GetTodos().ToPagedList(numPagina, tamPagina));
+            if(upload != null && upload.ContentLength > 0)
+            {
+                var arqImagem = new Cliente
+                {
+                    ImagemTipo = upload.ContentType
+                };
+                using(var reader = new BinaryReader(upload.InputStream))
+                {
+                    arqImagem.Imagem = reader.ReadBytes(upload.ContentLength);
+                }
+                cliente.Imagem = arqImagem.Imagem;
+                cliente.ImagemTipo = arqImagem.ImagemTipo;
+            }
+            db.Clientes.Add(cliente);
+            db.SaveChanges();
+            TempData["Mensagem"] = string.Format($"{cliente.Nome} incluído com sucesso!");
+
+
+            return RedirectToAction("Catalogo");
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ClienteId,Nome,Email,Endereco,Imagem,ImagemTipo")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Catalogo");
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cliente cliente = db.Clientes.Find(id);
+            db.Clientes.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Catalogo");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
